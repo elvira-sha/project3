fe429298a019380b7885ba9f3b0a37da63766c93
IpHo1 <ilya.cherny2007@gmail.com>
2024-10-02 21:56:20 +0300

    Устранение проблем с переводом строк

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
+class BmiService {
+    public float calculate(float weight, float height) {
+        return weight / (height * height);
 
-public class BonusMilesService {
+    }
 }
